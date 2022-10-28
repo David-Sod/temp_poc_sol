ed46af41dc1d0297d362c8c03b733138b9ab9245
David Rodriguez <david@stakeordie.com>
2022-10-28 12:55:31 -0700

    feat: first commit

---
+++ contract/contracts/contract.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Zombies is ERC721 {
+
+}
---
+++ contract/migrations/1_my_first_deploy.js
@@ -0,0 +1,8 @@
+var MyContract = artifacts.require("Zombies");
+module.exports = async function (deployer, _network, _account) {
+  // deploy a contract
+  await deployer.deploy(MyContract);
+  //access information about your deployed contract instance
+  // const instance = await MyContract.deployed();
+  // console.log(instance);
+};
